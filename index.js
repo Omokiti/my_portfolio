5cf544205d4d07307826ea2b5a6c5c2b520046bb
omokiti <eomokiti@yahoo.com>
2020-03-09 13:42:47 +0100

    add js file

---
+++ index.js
@@ -0,0 +1,22 @@
+let i = 0;
+let txt="Frontend Developer"
+let speed= 50;
+
+function effectWriter(){
+    if ( i < txt.length) {
+     document.getElementById("text").innerHTML += txt.charAt(i);
+     i++;
+     setTimeout(effectWriter,speed);
+   4000 }
+}
+
+effectWriter();
+var sample=[[0,1][2,3],[4,5]];
+var result = sample.reduce(function(a,b){
+    return a. concat(b);
+
+
+},[]);
+console.log(result);
+
+console.log(['1','2','1'].map(parseInt));
